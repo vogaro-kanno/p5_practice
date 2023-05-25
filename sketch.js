82795936f2dd7a18b2ecea4dc4d4981e35a47165
vogaro-kanno <k.kanno@vogaro.co.jp>
2023-05-25 17:39:39 +0900

    [add]ピカチュウ追加

--- sketch.js
+++ sketch.js
@@ -1,10 +1,48 @@
 function setup() {
-  createCanvas(windowWidth, windowHeight);
-  background(31, 127, 255);
+  createCanvas(640, 530)
 }
 
 function draw() {
-  noStroke();
-  fill(255, 255, 0);
-  circle(width / 2, height / 2, height / 2);
+  background(200)
+  noStroke()
+  noLoop()
+
+  // ear
+  fill(247, 255, 79)
+  triangle(187, 127, 255, 170, 224, 195)
+  triangle(310, 170, 420, 127, 360, 195)
+
+  fill(0)
+  triangle(187, 127, 205, 137, 207, 168)
+  triangle(390, 139, 420, 127, 384, 168)
+
+  // face
+  fill(247, 255, 79)
+  rect(193, 164, 193, 165, 100)
+
+  fill(0)
+  rect(207, 214, 17, 26, 80, 100, 100, 80)
+
+  fill(249, 69, 156)
+  rect(194, 255, 13, 20, 80, 100, 100, 80)
+
+  //右頬表示できない
+  // circle(326, 257, 23)
+
+  // body
+  fill(247, 255, 79)
+  ellipse(312, 300, 198, 234)
+
+  fill(234, 102, 63)
+  triangle(404, 253, 409, 272, 381, 298);
+  triangle(404, 283, 409, 302, 351, 328);
+
+  // arms
+  fill(247, 255, 79)
+  rect(200, 288, 29, 51, 30, 100, 100, 80);
+
+  // foot
+  fill(247, 255, 79)
+  rect(244, 397, 54, 36, 100, 100, 100, 100)
+  rect(338, 395, 33, 36, 100, 20, 50, 50)
 }
